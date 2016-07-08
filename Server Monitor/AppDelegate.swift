88bdaf1525e0c207200f1201adc47901c0274183
Spicer Matthews <spicer@cloudmanic.com>
2016-07-08 15:48:40 -0700

    Initial Commit

---
+++ Server Monitor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Server Monitor
+//
+//  Created by Spicer Matthews on 7/8/16.
+//  Copyright © 2016 Cloudmanic Labs, LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
